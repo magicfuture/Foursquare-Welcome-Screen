eb46be89fd2394a4788b58e33a4402861b40ab44
zacksheppard <zack@foursquare.com>
2012-09-04 00:12:11 -0400

    Modified the app to use GAE SDK 1.7.1

--- src/com/foursquare/examples/push/IndexServlet.java
+++ src/com/foursquare/examples/push/IndexServlet.java
@@ -30,9 +30,8 @@ public class IndexServlet extends HttpServlet {
       else {
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
-        
+
         FoursquareApi api = null;
-        
         if (req.getPathInfo().contains("user")) {
           String code = req.getParameter("code");
           if (code != null) {
@@ -47,7 +46,7 @@ public class IndexServlet extends HttpServlet {
             } catch (FoursquareApiException e) { api = null; }
           }
         } else api = Common.getCurrentApi(pm);
-        
+
         if (api == null || api.getOAuthToken() == null || api.getOAuthToken().length() <= 0) {
           writeHead(out, "Connect to foursquare");
           writeConnectPrompt(out);
@@ -62,7 +61,7 @@ public class IndexServlet extends HttpServlet {
       pm.close();
     }
   }
-  
+
   private void writeHead(PrintWriter out, String title) {
     out.println("<!doctype html><html>\n<head>\n<title>");
     out.println(title);
@@ -73,23 +72,23 @@ public class IndexServlet extends HttpServlet {
     out.println("<script type=\"text/javascript\" src=\"/static/script.js\"></script>");
     out.println("</head><body>\n<div id=\"wrapper\" class=\"container-fluid\">");
   }
-  
+
   private void writePage(PrintWriter out, String placeName) {
     out.println("<div id=\"lastCheckin\"><div id=\"picWrapper\"><img id=\"userPic\" src=\"\"/><img id=\"userCrown\" src=\"/static/crown.png\"/></div>");
     out.println("<h1 id=\"message\"></h1></div>\n<div id=\"herenow\">");
     out.println("<div id=\"people\"></div><h2><span id=\"count\">0</span> here @ "+placeName+"</h2>\n</div>");
   }
-  
+
   private void writeConnectPrompt(PrintWriter out) {
     out.println("<div id=\"connect\">\nTo use this widget, you need to connect to foursquare.<br />");
     out.print("<a href=\""+Common.getFoursquareLoginUrl());
     out.println("\"><img src=\"/static/connect-white.png\" /></a>\n</div>");
   }
-  
+
   // If producing a working page, include the channel token right away to get going immediately.
   private void writeFoot(PrintWriter out, String vid, String token) {
     out.println("<script type=\"text/javascript\">");
-    
+
     if (token != null) {
       out.println("channel = new goog.appengine.Channel('"+token+"');");
       out.println("socket = channel.open();\nsocket.onopen = onOpened;\nsocket.onmessage = onMessage;");
@@ -98,7 +97,7 @@ public class IndexServlet extends HttpServlet {
     if (vid != null) {
       out.println("var vid = '"+vid+"';");
     }
-    
+
     out.println("</script>");
 
     out.print("</div></body></html>");
--- src/com/foursquare/examples/push/util/Common.java
+++ src/com/foursquare/examples/push/util/Common.java
@@ -23,7 +23,7 @@ public class Common {
   public final static String PUSH_SECRET = "YOUR CLIENT PUSH SECRET";
   public final static String TARGET_VENUE = "THE FOURSQUARE ID OF THE VENUE YOU MANAGE";
   public final static String TARGET_VENUE_NAME = "THE NAME OF THE VENUE YOU MANAGE";
-  
+
   public static FoursquareApi getApi() { return getApi(null); }
   public static FoursquareApi getApi(String token) {
     FoursquareApi foursquareApi = new FoursquareApi(CLIENT_ID, CLIENT_SECRET, CALLBACK);
@@ -38,10 +38,10 @@ public class Common {
         return getApi(luser.foursquareAuth());
       }
     }
-    
+
     return null;
   }
-  
+
   // Convert the core aspects of a checkin to json
   public static String checkinToJson(String name, String photo, boolean isMayor) {
     StringBuilder sb = new StringBuilder();
@@ -54,7 +54,7 @@ public class Common {
     sb.append("}");
     return sb.toString();
   }
-  
+
   /* Methods for getting at aspects of persistence */
   private static final PersistenceManagerFactory pmfInstance =
       JDOHelper.getPersistenceManagerFactory("transactions-optional");
@@ -62,46 +62,46 @@ public class Common {
   private static PersistenceManagerFactory getPMF() {
     return pmfInstance;
   }
-  
+
   public static PersistenceManager getPM() {
     return getPMF().getPersistenceManager();
   }
-  
+
   /* Methods for user management tidbits */
   public static User getGoogleUser() {
     return UserServiceFactory.getUserService().getCurrentUser();
   }
-  
+
   public static String getGoogleLoginUrl() {
     return UserServiceFactory.getUserService().createLoginURL("/#retryAuth");
   }
-  
+
   public static String getFoursquareLoginUrl() {
     return "https://foursquare.com/oauth2/authenticate?client_id=" + CLIENT_ID +
            "&response_type=code&redirect_uri=" + CALLBACK;
   }
-  
+
   /* Methods for handling Channel Client ID creation and understanding */
   public static String createChannelToken(String vid) {
     ChannelService cService = ChannelServiceFactory.getChannelService();
     return cService.createChannel(getClientId(vid));
   }
-  
+
   private static String getClientId(String vid) {
     return vid + "-" + (new Date()).getTime();
   }
-  
+
   public static String parseClientId(String clientId) {
     int ind = clientId.indexOf('-');
     if (ind > 0) {
       return clientId.substring(0, ind);
     } else return null;
   }
-  
+
   // Actually handle sending out messages to a given list of clients
   public static void sendUpdate(List<String> clients, String message) {
     ChannelService cService = ChannelServiceFactory.getChannelService();
-    
+
     for (String client : clients) {
       cService.sendMessage(new ChannelMessage(client, message));
     }
